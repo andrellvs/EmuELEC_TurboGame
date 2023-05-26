35c9aa956188a62e52bf668040b1a67effe47c26
NinjaBrothers <andrellvs@hotmail.com>
2023-05-26 17:16:12 -0300

    ADICIONAR TEMAS AQUI!

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -1,4 +1,4 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
+## SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
