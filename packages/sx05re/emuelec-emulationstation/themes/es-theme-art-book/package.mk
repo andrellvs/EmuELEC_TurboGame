3ec2b72955f34efdcf1728c383f2993a59d60be8
NinjaBrothers <andrellvs@hotmail.com>
2023-05-26 21:30:17 -0300

    Create package.mk

---
+++ packages/sx05re/emuelec-emulationstation/themes/es-theme-art-book/package.mk
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
+
+PKG_NAME="es-theme-art-book"
+PKG_VERSION="a16c561f6d40310cac8029c2bf4c68ff6cdd9333"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/anthonycaccese/es-theme-art-book"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="emuelec"
+PKG_SHORTDESC="The EmulationStation theme Designed by anthonycaccese"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+PKG_TOOLCHAIN="manual"
+
+make_target() {
+  : not
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/config/emulationstation/themes/es-theme-art-book
+    cp -r * $INSTALL/usr/config/emulationstation/themes/es-theme-art-book
+}
