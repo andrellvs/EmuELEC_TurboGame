8cec4949cfa7e592173b2d272ef01975bf607a10
NinjaBrothers <andrellvs@hotmail.com>
2023-05-27 11:31:53 -0300

    Update package.mk

--- packages/sx05re/emuelec-emulationstation/themes/Crystal/package.mk
+++ packages/sx05re/emuelec-emulationstation/themes/Crystal/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="Crystal"
-PKG_VERSION="e7d7439b376a0e9e52a01b94d83a7479083b2fbe"
+PKG_VERSION="a18103de15b5537ea6d59e83af5530c2f0dc8bb8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
