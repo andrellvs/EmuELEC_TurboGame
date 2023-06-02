8e905acfbdde87a32c7f8cd2c74e5c6d7444bbe0
NinjaBrothers <andrellvs@hotmail.com>
2023-06-01 21:57:40 -0300

    Update package.mk

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -2,14 +2,14 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
-#PKG_VERSION="a70313633ef29b57e287efaeb6dfa7967684969b"
-PKG_VERSION="2f1385ac5f6387eb2d399f4bea1144930a42b610"
+PKG_VERSION="a70313633ef29b57e287efaeb6dfa7967684969b"
+#PKG_VERSION="2f1385ac5f6387eb2d399f4bea1144930a42b610"
 PKG_GIT_CLONE_BRANCH="EmuELEC"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-#PKG_SITE="https://github.com/EmuELEC/emuelec-emulationstation"
-PKG_SITE="https://github.com/andrellvs/emuelec-emulationstation"
+PKG_SITE="https://github.com/EmuELEC/emuelec-emulationstation"
+#PKG_SITE="https://github.com/andrellvs/emuelec-emulationstation"
 PKG_URL="$PKG_SITE.git"
 PKG_DEPENDS_TARGET="toolchain SDL2 freetype curl freeimage vlc bash rapidjson ${OPENGLES} SDL2_mixer fping p7zip"
 PKG_SECTION="emuelec"
