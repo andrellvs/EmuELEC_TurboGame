0a48cd3f47decfd3c6d223ca6a7a7151b11d2e81
NinjaBrothers <andrellvs@hotmail.com>
2023-06-08 15:14:28 -0300

    Update package.mk

--- packages/audio/speexdsp/package.mk
+++ packages/audio/speexdsp/package.mk
@@ -2,7 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="speexdsp"
-PKG_VERSION="1.2.1"
+PKG_VERSION="1b28a0f61bc31162979e1f26f3981fc3637095c8"
+PKG_GIT_CLONE_BRANCH="release/1.2.1"
 PKG_LICENSE="BSD"
 PKG_SITE="https://speex.org"
 PKG_URL="https://github.com/xiph/speexdsp/tree/release/1.2.1.git"
